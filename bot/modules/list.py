e3063563dc6ac8c9f1ac048b0f37ca46fb327cd4
Juan <72490350+jusidama18@users.noreply.github.com>
2021-06-02 06:26:02 +0700

    Fix for apostrophe error

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -10,18 +10,20 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 def list_drive(update,context):
     try:
         search = update.message.text.split(' ',maxsplit=1)[1]
-        LOGGER.info(f"Searching: {search}")
-        reply = sendMessage('Searching..... Please wait!', context.bot, update)
-        gdrive = GoogleDriveHelper(None)
-        msg, button = gdrive.drive_list(search)
+        if "'"in search:
+            search = search.replace("'", "\\'")
+            LOGGER.info(f"Searching: {search}")
+            reply = sendMessage('Searching..... Please wait!', context.bot, update)
+            gdrive = GoogleDriveHelper(None)
+            msg, button = gdrive.drive_list(search)
 
-        if button:
-            editMessage(msg, reply, button)
-        else:
-            editMessage('No result found', reply, button)
+            if button:
+                editMessage(msg, reply, button)
+            else:
+                editMessage('No result found', reply, button)
 
-    except IndexError:
-        sendMessage('Send a search key along with command', context.bot, update)
+        except IndexError:
+            sendMessage('Send a search key along with command', context.bot, update)
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_drive,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
